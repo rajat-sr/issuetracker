cf25a3c45e53836ab2dcf00c96e4cdc45f4f2f61
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-04 04:11:04 +0530

    Create Kanban Layout

--- client/src/components/Lifecycle/Lifecycle.js
+++ client/src/components/Lifecycle/Lifecycle.js
@@ -4,13 +4,27 @@ import IssueContainer from './../../containers/IssueContainer/IssueContainer';
 import NewIssue from './../NewIssue/NewIssue';
 import classes from './Lifecycle.module.css';
 
+const issues = [
+  { _id: 1, issue: 'abcd' },
+  { _id: 2, issue: 'asvas' },
+  { _id: 3, issue: 'abasfmjkas nvbiacd' },
+  { _id: 4, issue: 'abasmkicou yvsagjbhcnapsjchuaisbc kjnpajihocuasjcd' },
+  { _id: 5, issue: 'abasklicuhauvbcd' },
+  { _id: 6, issue: 'asfmniuvbydandvfdgbhnjmkiyutyabcd' },
+];
+
 const Lifecycle = props => (
-  <Card elevation={Elevation.ONE} className={classes.Lifecycle}>
+  <Card elevation={Elevation.TWO} className={classes.Lifecycle}>
     <h3>{props.name}</h3>
     <Divider />
-    {props.issues
-      ? props.issues.map(issue => <IssueContainer issue={issue} key={issue._id} />)
-      : null}
+    {issues ? (
+      <div>
+        {issues.map(issue => (
+          <IssueContainer issue={issue.issue} key={issue._id} />
+        ))}
+        <Divider />
+      </div>
+    ) : null}
     <NewIssue />
   </Card>
 );

a2a35d86d55e9dc24bf08e3eba0b419051cdbdb9
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-04 02:35:10 +0530

    Add db schemas

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import boards from './routes/boards';
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/boards/', boards);
 
 app.get('/', (req, res) => {

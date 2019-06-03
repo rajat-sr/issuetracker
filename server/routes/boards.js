7eb033809f6d8b007e00248576f47cd2831ea1d0
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-04 03:10:33 +0530

    Add GET /board and GET /board/:boardid routes

--- server/routes/boards.js
+++ server/routes/boards.js
@@ -1,9 +1,34 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import Board from './../model/board';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Server says Hi');
+app.get('/', async (req, res) => {
+  let boards;
+  try {
+    boards = await Board.find();
+  } catch (e) {
+    return res.status(503).send(e);
+  }
+  return res.send(boards);
 });
+
+app.get('/:boardID', async (req, res) => {
+  const { boardID } = req.query;
+  let boards;
+  try {
+    boards = await Board.find({ _id: boardID });
+  } catch (e) {
+    return res.status(503).send(e);
+  }
+  return res.send(boards);
+});
+
+// app.post('/', async (req, res) => {
+  // const { name, lifeCycles } = req.body;
+  // const dbStatus = await Board.
+// });
 
 module.exports = app;

426548905ded9b4729763cbae4ff7de2c3886c71
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-04 04:11:04 +0530

    Create Kanban Layout

--- client/src/containers/IssueContainer/IssueContainer.js
+++ client/src/containers/IssueContainer/IssueContainer.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Card, Elevation } from '@blueprintjs/core';
+import classes from './IssueContainer.module.css'
 
-const IssueContainer = () => {
+const IssueContainer = props => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Card className={classes.IssueContainer} elevation={Elevation.TWO}>
+      <p key={props._id}>{props.issue}</p>
+    </Card>
+  );
+};
 
-export default IssueContainer
+export default IssueContainer;

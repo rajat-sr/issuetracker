0e41b54d768bddd4554536e806379a0862ac5228
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-04 02:35:10 +0530

    Add db schemas

--- server/routes/boards.js
+++ server/routes/boards.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.status(200).send('Server says Hi');
 });

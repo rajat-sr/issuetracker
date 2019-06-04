94086d67388b2df9048e92f426d91292c2097d50
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-06-04 08:34:16 +0530

    Hit create new board api

--- client/src/components/BoardList/Board.js
+++ client/src/components/BoardList/Board.js
@@ -11,6 +11,10 @@ class Board extends React.Component {
     loadKanban: false,
     boards: null,
     kanban: null,
+    boardName: '',
+    lifecycle1: '',
+    lifecycle2: '',
+    lifecycle3: '',
   };
   componentDidMount() {
     axios
@@ -26,14 +30,51 @@ class Board extends React.Component {
       .get(`${SERVER_URL}/boards/${boardId}/issues`)
       .then(res => {
         this.setState({ kanban: res.data, loadKanban: true });
-        console.log(res.data)
+        console.log(res.data);
       })
       .catch();
   };
 
+  onChangeHandler = (event, whichLifecycle) => {
+    let change;
+    if (whichLifecycle === 0) {
+      change = { lifecycle1: event.target.value };
+    } else if (whichLifecycle === 1) {
+      change = { lifecycle2: event.target.value };
+    } else {
+      change = { lifecycle3: event.target.value };
+    }
+    this.setState(change);
+  };
+
+  onNameChangeHandler = (event) => {
+    this.setState({boardName: event.target.value})
+  }
+
+  createBoardHandler = () => {
+    axios.post(`${SERVER_URL}/boards`, {
+      name: this.state.boardName,
+      lifeCycles: [
+        {
+          name: this.state.lifecycle1,
+          sequenceNumber: 0
+        },
+        {
+          name: this.state.lifecycle2,
+          sequenceNumber: 0
+        },
+        {
+          name: this.state.lifecycle3,
+          sequenceNumber: 0
+        }
+      ]
+    }).then(res => {
+      
+    }).catch(e => console.log(e))
+  }
+
   render() {
     const { loadKanban, kanban } = this.state;
-    
     const layout = loadKanban ? (
       <KanbanLayout lifecycles={kanban.lifeCycles} />
     ) : (
@@ -41,9 +82,25 @@ class Board extends React.Component {
         <Card className={classes.container}>
           <h3>Create a New Board</h3>
           Board Name
-          <input type="text" />
-          <LifecycleImplemenation />
-          <Button text="Create New Board" />
+          <input type="text" onChange={event => this.onNameChangeHandler(event)}/>
+          {/* <LifecycleImplemenation /> */}
+          Lifecycles
+          <input
+            type="text"
+            placeholder="Lifecycle 1 Name"
+            onChange={event => this.onChangeHandler(event, 0)}
+          />
+          <input
+            type="text"
+            placeholder="Lifecycle 2 Name"
+            onChange={event => this.onChangeHandler(event, 1)}
+          />
+          <input
+            type="text"
+            placeholder="Lifecycle 3 Name"
+            onChange={event => this.onChangeHandler(event, 2)}
+          />
+          <Button text="Create New Board" onClick={() => this.createBoardHandler()}/>
         </Card>
         {this.state.boards ? (
           <Card className={classes.BoardList}>
